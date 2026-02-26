53bd251b6aa589068f08e7088e7fa89bd1da9ec7
Isaac <omodingisaac111@gmail.com>
2026-02-26 19:24:43 +0300

    Remove punkt_tab download, pin scikit-learn version

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ import nltk
 
 # Download NLTK data
 nltk.download('punkt', quiet=True)
-nltk.download('punkt_tab', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
